e58698d5b48154b220383878df03c2eecfdbc0ce
Huansha13 <ing.yfhl13@outlook.com>
2023-03-08 21:34:08 -0500

    holaMundo

---
+++ app/src/main/java/xyz/android/holamundo/MainActivity.java
@@ -0,0 +1,17 @@
+package xyz.android.holamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Hola mundo", Toast.LENGTH_SHORT).show();
+    }
+}
